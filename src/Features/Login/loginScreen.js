31d2119e9c31db99712c2c803bd52677e074f040
Kevin Le Perf <kevin.leperf@dolby.com>
2021-03-01 20:01:29 +0100

    Prevent crash on react-native 0.63 (+ bumping to 0.27.-BETA)

--- src/Features/Login/loginScreen.js
+++ src/Features/Login/loginScreen.js
@@ -4,32 +4,34 @@ import { View, Text } from 'react-native';
 import { LoginScreenStyles } from './loginScreenStyles';
 import { VoxeetSDK } from '@voxeet/react-native-voxeet-conferencekit';
 
+
+const loadCall = async () => {
+  const isHermes = () => !!global.HermesInternal;
+  console.log('Voxeet test issue hermes: ', isHermes());
+  await VoxeetSDK.initialize(
+    '<consumer_key>',
+    '<consumer_secret>'
+  );
+  await VoxeetSDK.connect({
+    externalId: 'test@gmail.com',
+    name: 'test@gmail.com',
+  });
+  const conferenceAlias = `76099784248324_QA_streamstyle`.toLocaleLowerCase();
+  const conference = await VoxeetSDK.create({ alias: conferenceAlias });
+  console.log('voxeet debug conferenceAlias: ', conferenceAlias);
+  console.log('voxeet debug conference: ', conference);
+  const joined = await VoxeetSDK.join(conference.conferenceId, {
+    constraints: {
+      audio: true,
+      video: true,
+    },
+  });
+  console.log('voxeet debug joined: ', joined);
+}
+
 function LoginScreen(props) {
   useEffect(() => {
-    const isHermes = () => !!global.HermesInternal;
-    console.log('Voxeet test issue hermes: ', isHermes());
-    VoxeetSDK.initialize(
-      '<consumer_key>',
-      '<consumer_secret>'
-    );
-    VoxeetSDK.connect({
-      externalId: 'test@gmail.com',
-      name: 'test@gmail.com',
-    })
-      .then(async () => {
-        const conferenceAlias = `76099784248324_QA_streamstyle`.toLocaleLowerCase();
-        const conference = await VoxeetSDK.create({ alias: conferenceAlias });
-        console.log('voxeet debug conferenceAlias: ', conferenceAlias);
-        console.log('voxeet debug conference: ', conference);
-        const joined = await VoxeetSDK.join(conference.conferenceId, {
-          constraints: {
-            audio: true,
-            video: true,
-          },
-        });
-        console.log('voxeet debug joined: ', joined);
-      })
-      .catch((e) => console.log('Voxeet join failed with error: ', e));
+    loadCall().catch((e) => console.log('Voxeet join failed with error: ', e));
   }, []);
 
   return (
